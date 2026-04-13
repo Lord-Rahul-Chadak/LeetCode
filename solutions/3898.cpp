062602e88b4d7074a95ff7c951b560d8e220910c
Lord-Rahul-Chadak <chadakrahul@gmail.com>
2026-04-13 19:26:51 +0000

    find_degree_of_each_vertex

---
+++ solutions/3898.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findDegrees(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        vector<int> res(n, 0);
+
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n; j++){
+                res[i] += matrix[i][j];
+            }
+        }
+
+        return res;
+    }
+};
